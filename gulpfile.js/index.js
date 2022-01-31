de0147b7206fffa0a8fa7bd1f03243abacf391b7
nmrspb <muratov168@gmail.com>
2022-02-01 01:05:35 +0300

    Оптимизация

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -3,37 +3,37 @@ const { watch, series, parallel } = require("gulp");
 //Пути
 const path = require("./g-config/path");
 
-
 //Таски
 const browserSync = require("browser-sync").create();
-const clear = require('./g-tasks/clear')
-const html = require('./g-tasks/html')
-const scss = require('./g-tasks/scss')
-const sprite = require('./g-tasks/sprite')
-const zip = require('./g-tasks/zip')
-const images = require('./g-tasks/image')
-const fonts = require('./g-tasks/font')
-const js = require('./g-tasks/js')
+const clear = require("./g-tasks/clear");
+const html = require("./g-tasks/html");
+const scss = require("./g-tasks/scss");
+const sprite = require("./g-tasks/sprite");
+const zip = require("./g-tasks/zip");
+const images = require("./g-tasks/image");
+const fonts = require("./g-tasks/font");
+const js = require("./g-tasks/js");
+const copy = require("./g-tasks/copy");
 
 //Сервер
 const server = () => {
-  browserSync.init({
-    server: {
-      baseDir: path.root,
-    },
-    open: false,
-  })
-}
+   browserSync.init({
+      server: {
+         baseDir: path.root,
+      },
+      open: false,
+   });
+};
 
 //Наблюдатель
 const watcher = () => {
-  watch(path.html.watch, html).on("all", browserSync.reload)
-  watch(path.scss.watch, scss).on("all", browserSync.reload)
-  watch(path.sprite.watch, sprite).on("all", browserSync.reload)
-  watch(path.image.watch, images).on("all", browserSync.reload)
-  watch(path.font.watch, fonts).on("all", browserSync.reload)
-  watch(path.js.watch, js).on("all", browserSync.reload)
-}
+   watch(path.html.watch, html).on("all", browserSync.reload);
+   watch(path.scss.watch, scss).on("all", browserSync.reload);
+   watch(path.sprite.watch, sprite).on("all", browserSync.reload);
+   watch(path.image.watch, images).on("all", browserSync.reload);
+   watch(path.font.watch, fonts).on("all", browserSync.reload);
+   watch(path.js.watch, js).on("all", browserSync.reload);
+};
 
 //Задачи
 exports.scss = scss;
@@ -43,8 +43,12 @@ exports.zip = zip;
 exports.images = images;
 exports.fonts = fonts;
 exports.js = js;
+exports.copy = copy;
 
-const build = series(clear, parallel(html, scss, sprite, images, fonts, js));
+const build = series(
+   clear,
+   parallel(html, scss, sprite, images, fonts, js, copy)
+);
 const dev = series(build, parallel(watcher, server));
 
 //Сборки
