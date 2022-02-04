bc07da1ff320a5aca0d95d87199766262e6c3e58
nmrspb <muratov168@gmail.com>
2022-02-04 23:40:54 +0300

    Добавлен tippy

---
+++ src/js/libs/_tippy.js
@@ -0,0 +1,9 @@
+import tippy from "tippy.js";
+
+if ((document.querySelector = "#block")) {
+   tippy("#block", {
+      content: "I'm a Tippy tooltip!", // Здесь текст подсказки
+      placement: "top", // Расположение
+      arrow: true, // Стрелка
+   });
+}
